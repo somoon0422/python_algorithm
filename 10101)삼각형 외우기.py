89c85c0fac8a5902e0f6f86802cbca420b4d0701
kimsohee <somoon0422@gmail.com>
2023-03-22 16:45:18 +0900

    230322

--- 10101)삼각형 외우기.py
+++ 10101)삼각형 외우기.py
@@ -2,6 +2,12 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-if a+b+c == 180:
-    if a == 60:
-        print("Equilateral")
+if a & b & c == 60:
+    print("Equilateral")
+elif a+b+c == 180:
+    if a != b != c:
+        print("Scalene")
+    else:
+        print("Isosceles")
+else:
+    print("Error")
