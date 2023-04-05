0311798a27b5b38ae069e71c58f712d0ec8f8627
kimsohee <somoon0422@gmail.com>
2023-04-05 17:28:19 +0900

    230405

--- 2753)윤년.py
+++ 2753)윤년.py
@@ -1,8 +1,7 @@
-leap_year = int(input())
+year = int(input())
 
-if leap_year % 4 == 0:
-    print("1")
-if leap_year % 100 == 0:
-    print("0")
+if year % 4 ==0 and year % 100 !=0 or year % 400 == 0:
+    print(1)
+else:
+    print(0)
 
-#미해결
