0c23e66ded494948c5306291f593f35796e8df43
kimsohee <somoon0422@gmail.com>
2023-04-17 17:30:52 +0900

    230417

--- 230410)용혜님 숙제.py
+++ 230410)용혜님 숙제.py
@@ -9,8 +9,8 @@ filextention = ('.bmp')
 for root, dirs, files in os.walk(file_path):
     for file in files:
         if file.endswith(filextention):
-            file_list.append(file)
+            file_list.append(file) #file_path의 .bmp파일들을 file_list에 저장
             
 for file in file_list:
     if file.endswith(filextention):
-        print(os.path.splitext(file)[0].split('_')[0])
+        print(os.path.splitext(file)[0].split('_')[0]) #file_list의 .bmp파일일경우 파일명에서 차번만 출력
--- 230412)지인님 숙제3.py
+++ 230412)지인님 숙제3.py
@@ -1,16 +1,26 @@
 import os # os 모듈을 가져옴
 
-exts=(".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG")  # 확장자명을 튜플로 저장 
+exts=(".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG",".bmp")  # 확장자명을 튜플로 저장 
 
-def read_files(path, exts): # path: 폴더 경로, exts: 확장자명 
+def read_files(path, exts): # path: 폴더경로, exts: 확장자명 
     files = []  # 파일 경로를 저장할 리스트
     
-    for r, d, f in os.walk(path):   # 폴더 경로, 폴더명, 파일명
-        for file in f: # 파일명  
-            if file.lower().endswith(exts): # 확장자명이 튜플에 있으면 
-                file = os.path.join(r, file) # 파일 경로를 생성 
-                file = os.path.abspath(file) # 절대 경로로 변환 
-                file = file.replace(os.sep, "/") # 경로 구분자를 "/"로 변경 
-                files.append(file) # 리스트에 추가 
+    for r, d, f in os.walk(path):   # root 폴더 경로, dirs폴더명, file파일명
+        for file in f: # path의 파일명을 하나씩 가져옴
+            if file.lower().endswith(exts): # 파일명이 확장자명으로 끝나면 
+                file = os.path.join(r, file) # join() 함수로 root와 file을 합침
+                file = os.path.abspath(file) # abspath() 함수로 절대경로를 읽어옴
+                file = file.replace(os.sep, "/") #os.sep으로 경로구분자를 "/"로 변경함.
+                files.append(file) # files 리스트에 for문을 돌면서 파일 경로를 저장함. 
             
-    return files # 파일 경로를 반환 
+    return files # 파일 경로를 반환
+
+read_files("C:/Users/kimsh-dt01/Downloads/PlateCut/Special_Plate", exts) # 파일 경로를 출력함.
+
+
+# 도전과제 1
+# 지금은 read_files() 함수 안에 넣는 인자가 하드코딩되어 있습니다.
+# argparse를 사용해서 인자를 받아서 처리하도록 수정해 보세요.
+
+# 도전과제 2
+# 도전과제 1의 결과에 더해서, 파일 대신 하위 폴더 목록을 출력하도록 수정해 보세요.
