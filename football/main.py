da6df6c526c951a9033a2096fa7eecc91c365304
kimsohee <somoon0422@gmail.com>
2023-05-19 13:25:57 +0900

    230519

--- football/main.py
+++ football/main.py
@@ -4,60 +4,98 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-import datetime
+from bs4 import BeautifulSoup
 import time
 import requests
 import re
-
-
+import datetime
 
 def reservation(court_num):
     # 크롬 드라이버 실행
     driver = webdriver.Chrome()
     
     # 축구장 예약 웹 사이트의 URL
-    url = 'http://www.futsalbase.com/login'
-    
-    # 대관예약 페이지로 이동
-    res = requests.get(url)
-    
+    driver.get("http://www.futsalbase.com/login")
     time.sleep(1)
 
-    # 로그인 처리
-    driver.find_element(By.ID, "id").send_keys("somoon0422")
-    driver.find_element(By.ID, "pwd").send_keys("gooddata001!@#")
-    click_button = driver.find_element(By.XPATH, '/html/body/div/div/div/section/div/div[2]/div[1]/div[2]/button')
-    click_button.click()
+    #로그인하기
+    xpath_username = '//*[@id="id"]' 
+    xpath_password = '//*[@id="pwd"]'
+    xpath_login = '/html/body/div/div/div/section/div/div[2]/div[1]/div[2]/button'
     
+    driver.find_element(By.XPATH, xpath_username).send_keys(username) # 아이디 입력
+    driver.find_element(By.XPATH, xpath_password).send_keys(password) # 비밀번호 입력
+    driver.find_element(By.XPATH, xpath_login).click() # 로그인 버튼 클릭
+    print("로그인 성공")
     time.sleep(2)
 
     #팝업창 닫기
-    try:
-        for window in driver.window_handles[1:]:
-            driver.switch_to.window(window) # 창으로 전환
-            driver.close() # 창 닫기
-        driver.switch_to.window(driver.window_handles[0]) # 메인 창으로 전환
-    except:
-        pass
+    for i in range(3):
+        try:
+            xpath_popup = '/html/body/div/div/div/div[3]/div/div/div/div[2]/button'
+            driver.find_element(By.XPATH, xpath_popup).click()
+        except:
+            pass
+    
 
     # 대관예약 페이지로 이동
-    reserve_button = driver.find_element(By.XPATH, '/html/body/div/div/div/section/div/ul/li[3]/a')
+    
+    reserve_button = driver.find_element(By.XPATH, '/html/body/div/div/div/section/div/ul/li[3]')
     reserve_button.click()
+    print("대관예약 페이지로 이동합니다.")
+    time.sleep(2)
     
-    # 축구장 목록 찾기
-    html = driver.page_source
-    courts = re.findall(r'<li class="ballpark-list">(.*?)</li>', html)
-    
-    # 선택한 구장 번호에 해당하는 구장 찾기
-    for court in courts:
-        if court.find("a").text == "{}구장".format(count_num):
-            court_url = court.find("a")["href"]
+    #대관예약 페이지 클릭
+    for i in range(8):
+        xpath_court_link= '/html/body/div/div/div/div[2]/div[2]/ul/li[{}]/a'.format(i) # 축구장 링크 xpath
+        if i == court_num+1 :
+            driver.find_element(By.XPATH, xpath_court_link).click()
             break
     
-    # 해당하는 구장의 링크 클릭
-    res = requests.get(court_url)
+    next_month = "/html/body/div/div/div/div[2]/div[2]/div[1]/div[1]/div/div/div[2]/div/div/div[1]/a[2]"
+    driver.find_element(By.XPATH, next_month).click()
+    
+    
+    #여기서부터 잘 안됨.....
+    
+    # 오늘 날짜를 가져옵니다.
+    # 예약 가능한 날짜 확인
+    for i in range(2,7): # 2~6주차
+        for day in range(1,8): # 1~7일
+            xpath_month = '/html/body/div/div/div/div[2]/div[2]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div[2]/div/table/tbody/tr/td/div' #전체화면
+            xpath_week = driver.find_element(By.XPATH, '/html/body/div/div/div/div[2]/div[2]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div[2]/div/table/tbody/tr/td/div/div/div[{}]'.format(i))#1주일단위 
+            #날짜가 오늘날짜 이후일 경우 진행, 아닐 경우 pass
+            xpath_date ='/html/body/div/div/div/div[2]/div[2]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div[2]/div/table/tbody/tr/td/div/div/div[{}]/div[2]/table/thead/tr/td[{}]/span'.format(i,day)
+            today = datetime.today()
+            today_str = today.strftime("%Y-%m-%d")
+        
+        for day in range(1,8):
+            xpath_day1 = '/html/body/div/div/div/div[2]/div[2]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div[2]/div/table/tbody/tr/td/div/div/div[1]/div[3]/table/tbody/tr/td[{}]'.format(day)
+            xpath_day2 = '/html/body/div/div/div/div[2]/div[2]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div/div[2]/div/table/tbody/tr/td/div/div/div[1]/div[2]/table/tbody/tr/td[{}]/a/div[1]/span'.format(day)
+            check_box = '//*[@id="pushYn"]'
+            #action
+            #날짜를 하나씩 클릭
+            driver.find_element(By.XPATH, xpath_day1).click()
+            driver.find_element(By.XPATH, xpath_day2).click()
+            print("{}일을 조회합니다.".formaty(day))
+            time.sleep(1)
+            
+            try:
+                target_xpath = '/html/body/div/div/div/div[2]/div[2]/table/tbody/tr[8]/td[1]/label'
+                if target_xpath in 'label':
+                    driver.find_element(By.XPATH, check_box).click()
+                    print("예약 가능한 날짜입니다.")
+            except:
+                print("예약 불가능한 날짜입니다.")
+                pass
+
+
+            path = /html/body/div/div/div/div[2]/div[2]/table/tbody/tr[10]/td[1]/label에 접근해봐서 성공하면 클릭하기. 실패하면 pass
+        
 
 
 if __name__ == '__main__':
-    count_num = int(input("예약하실 축구장 번호를 입력하세요: ")) # ex) 4
+    count_num = int(input("예약하실 축구장 번호를 입력하세요: ")) # ex) 4  
+    username = input("아이디를 입력하세요: ")
+    password = input("비밀번호를 입력하세요: ")
     reservation(count_num)
---
+++ 오빠꺼.py
@@ -0,0 +1,124 @@
+from bs4 import BeautifulSoup # HTML을 파싱하는 라이브러리
+import urllib.request as req # url을 열고 html을 읽어오는 라이브러리
+from selenium import webdriver # 웹드라이버를 사용하기 위한 라이브러리
+from time import sleep # sleep을 사용하기 위한 라이브러리
+from selenium.webdriver.common.by import By # 웹드라이버에서 By를 사용하기 위한 라이브러리
+import pandas as pd # 데이터프레임을 사용하기 위한 라이브러리
+​
+def crawler(search_key): # 크롤링 함수
+    # 크롬드라이버 사용 지정
+    driver = webdriver.Chrome('./chromedriver')
+​
+    # HTML 가져오기
+    url = "https://www.foodsafetykorea.go.kr/portal/specialinfo/searchInfoProduct.do?menu_grp=MENU_NEW04&menu_no=2815#page1"
+​
+    # 크롬드라이버로 url 열기
+    driver.get(url)
+​
+    # 검색어 입력    
+    search_box = driver.find_element(By.XPATH, r'//*[@id="prd_nm"]').send_keys(search_key) # search_box에 search_key(검색어) 넣어줌
+​
+    # 페이지 로딩 기다리기
+    sleep(1)
+​
+    # 검색 버튼 클릭
+    search_btn = driver.find_element(By.CSS_SELECTOR, '#mode1 > div > span') # 버튼 지정
+    search_btn.click() # 지정한 버튼 클릭
+​
+    # 전체 제품의 성분 및 원료 저장할 리스트
+    product = []
+​
+    # html에서 원하는 데이터를 뽑아내는 과정
+    for page in range(0, search_pages): # 페이지 수 만큼 반복
+        # 페이지 로딩 기다리기
+        sleep(30)
+​
+        try: # 페이지가 1페이지일 경우 예외처리
+            for i in range(1, 11): # 페이지당 10개의 항목이 있으므로 10번 반복
+                # 한 소스의 정보를 저장할 리스트
+                product_tmp = []
+​
+                try: # 마지막 페이지에는 10개 항목 이하가 있을 수 있으므로 예외처리
+                    # 현재 페이지 소스 가져오기
+                    req = driver.page_source # 지금 브라우저 화면의 페이지 소스를 가져옴
+                    soup = BeautifulSoup(req, 'html.parser')  # 가져온 정보를 beautifulsoup로 파싱
+                    
+                    # 제품명 클릭
+                    elements = '#tbody > tr:nth-child({0}) > td:nth-child(6) > span.table_txt'.format(i)
+                    elements_text = driver.find_element(By.CSS_SELECTOR, elements).text.strip() # 제품명 가져오기
+                    search_btn = driver.find_element(By.CSS_SELECTOR, elements) # 버튼 지정
+                    search_btn.click() # 지정한 버튼 클릭
+                    sleep(2)
+​
+                    # 현재 페이지 소스 가져오기
+                    req = driver.page_source # 지금 브라우저 화면의 페이지 소스를 가져옴
+                    soup = BeautifulSoup(req, 'html.parser')  # 가져온 정보를 beautifulsoup로 파싱
+                    
+                    # 제품명의 성분 및 원료 따오기
+                    for idx in range(1, 1000):
+                        # 성분 및 원료 항목 가져오기
+                        element = 'body > div.fancybox-wrap.fancybox-desktop.fancybox-type-ajax.fancybox-opened > div > div > div > div.page-container > div.fcs_ddt > table.col.table-sm > tbody > tr:nth-child({0}) > td:nth-child(2)'.format(idx)
+​
+                        # 가져온 항목이 비어있지 않으면 리스트에 추가
+                        if str(soup.select(element)) != '[]':
+                            # 가져온 항목을 리스트에 추가
+                            product_tmp.append(str(soup.select(element)))
+                            
+                            # 리스트에 저장된 항목의 불필요한 부분 제거
+                            product_tmp = [i.strip('[<td>') for i in product_tmp]
+                            product_tmp = [i.strip('</td>]') for i in product_tmp]
+                            
+                            #한번에 작성 예시
+                            #product_tmp.append(str(soup.select(element)).strip('[<td>').strip('</td>]'))
+​
+​
+                        else: # 가져온 항목이 비어있으면 반복문 종료
+                            break
+                    
+                   
+                    
+                    # 한 제품의 성분 및 원료를 전체 제품의 성분 및 원료 리스트에 추가
+                    product.extend([elements_text,product_tmp])
+​
+                    # 뒤로가기 (X버튼 클릭)
+                    search_btn = driver.find_element(By.CSS_SELECTOR, r'#close') # 버튼 지정
+                    search_btn.click() # 지정한 버튼 클릭
+                    sleep(1)
+​
+                    print(f'{page+1}번째 페이지 {i}번째 항목 완료')
+​
+                except: # 마지막 페이지에는 10개 항목 이하가 있을 수 있으므로 예외처리
+                    break
+​
+            # 현재 페이지 소스 가져오기
+            req = driver.page_source # 지금 브라우저 화면의 페이지 소스를 가져옴
+            soup = BeautifulSoup(req, 'html.parser')  # 가져온 정보를 beautifulsoup로 파싱  
+            
+            # 다음 페이지로 넘어가기
+            element = '#contents > main > section > div.list-container > div.board-footer > div > ul > li:nth-child({0})'.format(page+3) # 다음 페이지 버튼의 XPath 지정
+            search_btn = driver.find_element(By.CSS_SELECTOR, element) # 버튼 지정
+            search_btn.click() # 지정한 버튼 클릭
+​
+            print(f'{page+1}번째 페이지 완료')
+​
+        except:
+            print('Paging Error!!!')
+            break
+​
+    print('\n<<product >>')
+    print(product)
+    print('-- Parshing Complete --')
+    driver.close() #드라이버를 닫아준다.
+​
+    # 크롤링 내역 csv로 저장
+    df = pd.DataFrame(data = product) 
+    df.to_csv('./product.csv', encoding='utf-8-sig') # csv로 저장
+    print('-- CSV Save Complete --')
+    
+    return product
+​
+if __name__ == '__main__':
+    search_key = input('검색어를 입력하세요 : ')
+    search_pages = int(input('탐색할 페이지를 입력하세요 : '))
+    crawler(search_key)
+    print('-- Crawling Complete --')
