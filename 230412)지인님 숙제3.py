2dcd5afeb4e788aa602c0458433c76ed43b2f960
somoon0422 <116736551+somoon0422@users.noreply.github.com>
2023-07-14 13:00:00 +0900

    230713

--- 230412)지인님 숙제3.py
+++ 230412)지인님 숙제3.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # import os # os 모듈을 가져옴
 
 # exts=(".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG",".bmp")  # 확장자명을 튜플로 저장 
@@ -16,12 +17,32 @@
 #     return files # 파일 경로를 반환
 
 # read_files("C:/Users/kimsh-dt01/Downloads/PlateCut/Special_Plate", exts) # 파일 경로를 출력함.
+=======
+import os  # os 모듈을 가져옴
+
+exts = (".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG", ".bmp")  # 확장자명을 튜플로 저장
+
+
+def read_files(path, exts):  # path: 폴더경로, exts: 확장자명
+    files = []  # 파일 경로를 저장할 리스트
+
+    for r, d, f in os.walk(path):  # root 폴더 경로, dirs폴더명, file파일명
+        for file in f:  # path의 파일명을 하나씩 가져옴
+            if file.lower().endswith(exts):  # 파일명이 확장자명으로 끝나면
+                file = os.path.join(r, file)  # join() 함수로 root와 file을 합침
+                file = os.path.abspath(file)  # abspath() 함수로 절대경로를 읽어옴
+                file = file.replace(os.sep, "/")  # os.sep으로 경로구분자를 "/"로 변경함.
+                files.append(file)  # files 리스트에 for문을 돌면서 파일 경로를 저장함.
+
+    return files  # 파일 경로를 반환
+>>>>>>> Stashed changes
 
 
 # 도전과제 1
 # 지금은 read_files() 함수 안에 넣는 인자가 하드코딩되어 있습니다.
 # argparse를 사용해서 인자를 받아서 처리하도록 수정해 보세요.
 
+<<<<<<< Updated upstream
 import os
 import argparse
 
@@ -68,6 +89,17 @@ folders_text = "\n".join(folders_list)
 print(folders_text)
 
 
+=======
+import argparse
+import os
+
+exts = (".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG", ".bmp")
+
+parser = argparse.ArgumentParser(description="Argparse homework")
+# add_arguments
+
+read_files("C:/Users/kimsh-dt01/Downloads/PlateCut/Special_Plate", exts)  # 파일 경로를 출력함.
+>>>>>>> Stashed changes
 
 
 # 도전과제 2
--- 25304)영수증.py
+++ 25304)영수증.py
@@ -1,15 +1,15 @@
-total_cost = int(input())  #총 가격
-total_objects = int(input()) #총 물건 개수
+total_cost = int(input())  # 총 가격
+total_objects = int(input())  # 총 물건 개수
 total = 0
 
-for i in range(1,total_objects+1):
-    object_cost, num_object = map(int,input().split()) #물건 가격
+for i in range(total_objects):
+    object_cost, num_object = map(int, input().split())  # 물건 가격
     total += object_cost * num_object
 
 if total_cost == total:
-    print("yes")
+    print("Yes")
 else:
-    print("no")
-    
-        
-#미해결
+    print("No")
+
+
+# 미해결
